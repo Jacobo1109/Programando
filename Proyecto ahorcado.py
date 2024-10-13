6d29b25c0b05aa666ba7b2db2b8611f4d0da7050
Jacobo1109 <a01708395@tec.mx>
2024-10-12 22:17:29 -0600

    Add files via upload

--- Proyecto ahorcado.py
+++ Proyecto ahorcado.py
@@ -1,11 +1,29 @@
 import random
+def archivos():
+    file = open("words.txt", "w+")
+    words = ["basketball\n", "volleyball\n", "futball\n", "soccer\n", "tennis\n", "beisbol\n", "padel\n", "squash\n", "natacion\n", "baile\n", "ajedrez\n", "esgrima\n", "equitacion\n", "polo\n", "clavados\n", "atletismo\n", "gimnasia\n", "badminton\n", "pingpong\n", "boxeo\n", "taekwondo\n", "surf\n", "ciclismo\n", "criquet\n", "patinaje\n", "waterpolo\n", "kayak\n", "rugby\n", "criquet\n", "mma\n", "esqui\n", "americano\n", "balonmano\n", "netball\n", "cheerleading\n", "softball\n", "canotaje\n", "bmx\n", "parkour\n", "capoeira\n", "kickboxing\n", "bolos\n", "billar\n", "skateboarding\n", "motocross\n", "snowboard\n", "judo\n", "karate\n", "triatlon\n", "remo\n", "paracaidismo\n", "paintball\n"]
+    file.writelines(words)
+    file.seek(0)
+    contenido = file.read()
+    print(contenido)
+    file.close()
 def ahorcado():
-    won = 0 
+    file = open("words.txt", "r")
+    won = 0
     lives = 6
-    guessed = []
+    guessed = []    
     ahorcado_dibujo = ['''
        +---+
        |   |
+           |
+           |
+           |
+           |
+     _______
+       ''',
+                       '''
+       +---+
+       |   |
        O   |
            |
            |
@@ -25,7 +43,7 @@ def ahorcado():
        +---+
        |   |
        O   |
-       |\  |
+      /|   |
            |
            |
      _______
@@ -48,7 +66,8 @@ def ahorcado():
            |
      _______
        ''']
-    lost=('''
+    
+    lost = ('''
        +---+
        |   |
        O   |
@@ -57,47 +76,97 @@ def ahorcado():
            |
      _______
        ''')
-    w=('''
+
+    w = ('''
        O   
       /|\  
       / \ 
      ''')
-    words = ["basketball","volleyball","futball","soccer","tennis","padel","squash","natacion","baile","ajedrez","esgrima", "equitacion","polo","clavados","atletismo","gimnasia","badminton","pingpong","boxeo","taekwondo","surf","ciclismo","criquet","patinaje","waterpolo","rugby",]
-
-    chosen_word = random.choice(words)
-
-    print("\nLa palabra tiene",len(chosen_word),"letras")
-
+    words = file.readlines()
+    chosen_word = random.choice(words).strip()
+    print("\nLa palabra tiene", len(chosen_word), "letras")
     for letter in chosen_word:
-      guessed.append("_")
-
+        guessed.append("_")
     while won == 0:
-      guess = input("\nAdivina una letra: ").lower()
-
-      for position in range(len(chosen_word)):
-        letter = chosen_word[position]
-        if letter == guess:
-          guessed[position] = letter
-      print(guessed)
-
-      if "_" not in guessed:
-        won = 1
-        print("Ganaste!", w)
-
-      if guess not in chosen_word:
-        lives -= 1
+        guess = input("\nAdivina una letra: ").lower()
+        if guess not in chosen_word:
+            lives -= 1
+            print(ahorcado_dibujo[6 - lives - 1])
+        for position in range(len(chosen_word)):
+            letter = chosen_word[position]
+            if letter == guess:
+                guessed[position] = letter        
+        print("Progreso:", guessed)
+        if "_" not in guessed:
+            won = 1
+            print("¡Ganaste!", w)
         if lives == 0:
-          won = 1
-          print("Perdiste!", lost)
-
-      print(lives)
-
-    print("La palabra era:",chosen_word,"!")
+            won = 1
+            print("¡Perdiste!", lost)
+            print("La palabra era:", chosen_word, "!")
+        print("Vidas restantes:", lives)
+    file.close()
 def CasoPrueba():
+    file = open("words.txt", "r")
     won = 0 
     lives = 6
-    guessed = []
-    lost=('''
+    guessed = []    
+    ahorcado_dibujo = ['''
+       +---+
+       |   |
+           |
+           |
+           |
+           |
+     _______
+       ''',
+                       '''
+       +---+
+       |   |
+       O   |
+           |
+           |
+           |
+     _______
+       ''',
+                       '''
+       +---+
+       |   |
+       O   |
+       |   |
+           |
+           |
+     _______
+       ''',
+                       '''
+       +---+
+       |   |
+       O   |
+      /|   |
+           |
+           |
+     _______
+       ''',
+                       '''
+       +---+
+       |   |
+       O   |
+      /|\  |
+           |
+           |
+     _______
+       ''',
+                       '''
+       +---+
+       |   |
+       O   |
+      /|\  |
+      /    |
+           |
+     _______
+       ''']
+    
+    lost = ('''
        +---+
        |   |
        O   |
@@ -106,53 +175,44 @@ def CasoPrueba():
            |
      _______
        ''')
-    w=('''
+
+    w = ('''
        O   
       /|\  
       / \ 
      ''')
-    words = ["basketball","volleyball","futball","soccer","tennis","padel","squash","natacion","baile","ajedrez","esgrima", "equitacion","polo","clavados","atletismo","gimnasia","badminton","pingpong","boxeo","taekwondo","surf","ciclismo","criquet","patinaje","waterpolo","rugby",]
-
-    chosen_word = random.choice(words)
-
-    print("\nLa palabra tiene",len(chosen_word),"letras")
-
+    words = file.readlines()
+    chosen_word = random.choice(words).strip()
+    print("\nLa palabra tiene", len(chosen_word), "letras")
     for letter in chosen_word:
-      guessed.append("_")
-      
-      letters = set(chosen_word)
-      
-      abc = list('abcdefghijklmnopqrstuvwxyz')
-      
-      random.shuffle(abc)
-
+        guessed.append("_")      
+    abc = list('abcdefghijklmnopqrstuvwxyz')      
+    random.shuffle(abc)
     while won == 0:
-      guess = abc.pop()
-      print("\n palabra addivinada:")
-
-      for position in range(len(chosen_word)):
-        letter = chosen_word[position]
-        if letter == guess:
-          guessed[position] = letter
-      print("Progreso",guessed)
-
-      if "_" not in guessed:
-        won = 1
-        print("Ganaste!", w)
-
-      if guess not in chosen_word:
-        lives -= 1
+        guess = abc.pop()
+        print("\nLetra adivinada:", guess)
+        if guess not in chosen_word:
+            lives -= 1
+            print(ahorcado_dibujo[6 - lives - 1])
+        for position in range(len(chosen_word)):
+            letter = chosen_word[position]
+            if letter == guess:
+                guessed[position] = letter      
+        print("Progreso:", guessed)
+        if "_" not in guessed:
+            won = 1
+            print("¡Ganaste!", w)
         if lives == 0:
-          won = 1
-          print("Perdiste!", lost)
-
-      print(lives)
-
-    print("La palabra era:",chosen_word,"!")
+            won = 1
+            print("¡Perdiste!", lost)
+            print("La palabra era:", chosen_word, "!")
+        print("Vidas restantes:", lives)
+    file.close()
 def menu():
     print("\n1 Jugar ahorcado")
     print("2 Caso Prueba")
     print("3 Salir")
+    print("4 Archivos")
 def main():
     continuar = True
     while continuar:
@@ -160,13 +220,15 @@ def main():
         opcion = str(input("\nFavor de Introducir una opción:"))
         if opcion == '1':
             print("\nBienvenido a Ahorcado")
+            print("\n¡¡¡La temática es deportes!!!")
             ahorcado()
         elif opcion == '2':
             CasoPrueba()
         elif opcion == '3':
             print("\nGracias por jugar Ahorcado")
             continuar = False
+        elif opcion == '4':
+            archivos()
         else:
             print("\nFavor de ingresar una opción válida")
 main()
-            
